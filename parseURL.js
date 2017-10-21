9d624eda603296ed62b8d4e140fe139e1fef8b33
alberthorta <albert@horta.name>
2017-10-21 12:03:22 +0200

    Refactor and comment the library + add tests

--- parseURL.js
+++ parseURL.js
@@ -1,29 +1,65 @@
+/**
+ * Parses Domain part of an URI.
+ * 
+ * @param {object} out_obj Containing the object that will be used to return the parsing in a form:
+ *        out_obj = {
+ *        	user     : {string | undefined} Username, 
+ *          password : {string | undefined} Password,  
+ *          domain   : {string | undefined} Domain name, 
+ *          port     : {string | undefined} Port
+ *        }
+ * @return {object} out_obj
+ */
+String.prototype.parseURIDomain = function(out_obj) {
+	var domain_components, domain_and_port, user_and_password;
+	out_obj.user = undefined; out_obj.password = undefined; out_obj.port = undefined; out_obj.domain = undefined;
+	out_obj.port = (out_obj.protocol === 'http'? 80: (out_obj.protocol === 'https'? 443: (out_obj.protocol === 'ftp'? 21: undefined)));
+	if(this && this.length > 0) {
+	    domain_components = ("@"+this).split("@");
+		domain_and_port = domain_components.pop().split(":",2);
+		out_obj.domain = domain_and_port[0]!=""? domain_and_port[0]: undefined;
+		out_obj.port = (domain_and_port[1]!=undefined)? parseInt(domain_and_port[1]): out_obj.port;
+		user_and_password = domain_components.pop().split(":",2);
+		out_obj.user = user_and_password[0]!=""? user_and_password[0]: undefined;
+		out_obj.password = user_and_password[1];
+	}
+	return out_obj;
+};
+
+/**
+ * Parses URI Params part of an URI.
+ *
+ * @return {object} Containing the parsed params.
+ */   
+String.prototype.parseURIParams = function() {
+	return decodeURIComponent(this).replace(/\+/g, ' ').split("&").reduce(function(out, current_value, current_index, initial_array) {
+		var param = initial_array[current_index].split("=",2);
+		out[param.shift()] = param.pop();
+		return out;
+	}, {});
+};
+
+/**
+ * Parses full URL
+ *
+ * @return {object} Containing the parsed URL in a form: 
+ *        {
+ *        	user     : {string | undefined} Username, 
+ *          password : {string | undefined} Password,  
+ *          domain   : {string | undefined} Domain name, 
+ *          port     : {string | undefined} Port,
+ *          params   : {object | undefined} Parsed GET Parameters,
+ *          path     : {string | undefined} Absolute path,
+ *          protocol : {string | undefined} Protocol ("http" | "https" | "file" | ... ) <lowercase>,
+ *          hash     : {string | undefined} Hash
+ *        }
+ */ 
 String.prototype.parseURL = function() {
 	var uri_components = (/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/g).exec(this);
-    var domain = undefined, port = undefined, user = undefined, password = undefined;
-	if(uri_components[4] != undefined) {
-	    var domain_components = ("@"+uri_components[4]).split("@");
-		var domain_and_port = domain_components.pop().split(":",2);
-		domain = domain_and_port[0];
-		port = domain_and_port[1];
-		var user_and_password = domain_components.pop().split(":",2);
-		user = user_and_password[0];
-		password = user_and_password[1];
-	}
-	var params_array = uri_components[7]===undefined?[]:decodeURIComponent(uri_components[7]).replace(/\+/g, ' ').split("&");
-	var params = {};
-	for(var i in params_array) {
-		var param = params_array[i].split("=",2);
-		params[param.shift()] = param.pop();
-	}
-    return { 
-		protocol : uri_components[2],
-		user     : user!=""?user:undefined,
-		password : password,
-		domain   : domain!=""?domain:undefined,
-		port     : (port!=undefined?parseInt(port):(uri_components[2]==='http'?80:(uri_components[2]==='https'?443:undefined))),
-		path     : uri_components[2]==='file'?uri_components[5].slice(1):uri_components[5],
-		params   : params,
-		hash     : uri_components[9]
-	};
+	return (uri_components[4] || "").parseURIDomain({ 
+		protocol : uri_components[2] !== undefined? uri_components[2].toLowerCase(): undefined,
+		path     : (uri_components[2] || "").toLowerCase()==='file'?uri_components[5].slice(1):uri_components[5],
+		hash     : uri_components[9],
+		params   : (!uri_components[7] || uri_components[7].length === 0)? {}: uri_components[7].parseURIParams()
+	});
 };
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,71 @@
+describe('String.prototype.parseURIDomain', function(){
+  var common_tests = function(parsed_domain) {
+  	expect(parsed_domain).to.be.an('object');
+  	expect(parsed_domain).to.only.have.keys('protocol','user','password','domain','port');  	
+  }
+  it('works on empty strings', function() {
+	var parsed_domain = "".parseURIDomain({protocol: undefined});
+	common_tests(parsed_domain);
+	expect(parsed_domain.user).to.be(undefined);
+	expect(parsed_domain.password).to.be(undefined);
+	expect(parsed_domain.domain).to.be(undefined);
+	expect(parsed_domain.port).to.be(undefined);
+  });
+  it('works on only domain strings', function() {
+  	var parsed_domain = "thats.the.domain.com".parseURIDomain({protocol: 'ftp'});
+  	common_tests(parsed_domain);
+	expect(parsed_domain.domain).to.be('thats.the.domain.com');
+	expect(parsed_domain.user).to.be(undefined);
+	expect(parsed_domain.password).to.be(undefined);
+	expect(parsed_domain.port).to.be(21);
+  });
+  it('works when sending domain:port strings', function() {
+  	var parsed_domain = "thats.the.domain.com:6767".parseURIDomain({protocol: 'http'});
+  	common_tests(parsed_domain);
+	expect(parsed_domain.domain).to.be('thats.the.domain.com');
+	expect(parsed_domain.user).to.be(undefined);
+	expect(parsed_domain.password).to.be(undefined);
+	expect(parsed_domain.port).to.be(6767);
+  });
+  it('works when sending user:password@domain strings', function() {
+  	var parsed_domain = "user:password@thats.the.domain.com".parseURIDomain({protocol: 'https'});
+  	common_tests(parsed_domain);
+	expect(parsed_domain.domain).to.be('thats.the.domain.com');
+	expect(parsed_domain.user).to.be('user');
+	expect(parsed_domain.password).to.be('password');
+	expect(parsed_domain.port).to.be(443);
+  });
+  it('works when sending user:password@domain:port strings', function() {
+  	var parsed_domain = "user:password@thats.the.domain.com:78".parseURIDomain({protocol: 'https'});
+  	common_tests(parsed_domain);
+	expect(parsed_domain.domain).to.be('thats.the.domain.com');
+	expect(parsed_domain.user).to.be('user');
+	expect(parsed_domain.password).to.be('password');
+	expect(parsed_domain.port).to.be(78);
+  });
+});
+
+describe('String.prototype.parseURIParams', function() {
+  it('works on empty string', function() {
+	var parsed_params = "".parseURIParams();
+	expect(parsed_params).to.be.an('object');
+  });
+  it('works on regular params', function() {
+	var parsed_params = "a&b=1&c=hello+world".parseURIParams();
+	expect(parsed_params).to.be.an('object');
+	expect(parsed_params).to.only.have.keys('a','b','c');
+	expect(parsed_params.a).to.be(undefined);
+	expect(parsed_params.b).to.be('1');
+	expect(parsed_params.c).to.be('hello world');
+  });
+  it('works on encoded params', function() {
+	var parsed_params = "a=%C3%A4%C3%AB%C3%AF%C3%B6%C3%BC%C3%A1%C3%A9%C3%AD%C3%B3%C3%BA%E6%B1%89%E5%AD%97".parseURIParams();
+	expect(parsed_params).to.be.an('object');
+	expect(parsed_params).to.only.have.keys('a');
+	expect(parsed_params.a).to.be("äëïöüáéíóú汉字");
+  });
+});
+
 describe('String.prototype.parseURL', function(){
   it('works on full URLS', function(){
     // just for example of tested value
